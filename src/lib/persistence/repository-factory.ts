9b3a8b99e74befecb3404379a8b2ea58c21b352e
ychebotaev <yury.79120345101@gmail.com>
2025-06-12 17:15:20 +0500

    fix: factory fix

--- src/lib/persistence/repository-factory.ts
+++ src/lib/persistence/repository-factory.ts
@@ -1,13 +1,15 @@
 import { Logger } from "pino";
 import { OidcTypes } from "../adapter";
 import { KnexDatabase } from "./knex-database";
-import * as Repositories from "./repositories";
+import * as repositories from "./repositories";
 
 export class RepositoryFactory {
   constructor(private database: KnexDatabase, private logger: Logger) {}
 
   public create(name: keyof typeof OidcTypes) {
-    const Repository = Repositories[name];
+    const [Repository] = Object.entries(repositories)
+      .filter(([className]) => className.startsWith(name))
+      .map(([_, Ctor]) => Ctor);
 
     return new Repository(this.database, this.logger);
   }

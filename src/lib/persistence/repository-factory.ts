d2eb4e57b4eb64d25858bf6c815771713c77f01c
ychebotaev <yury.79120345101@gmail.com>
2025-06-15 21:22:09 +0500

    feat: cache repository in factory

--- src/lib/persistence/repository-factory.ts
+++ src/lib/persistence/repository-factory.ts
@@ -2,15 +2,24 @@ import { Logger } from "pino";
 import { OidcTypes } from "../adapter";
 import { KnexDatabase } from "./knex-database";
 import * as repositories from "./repositories";
+import type { RepositoryBase } from "./repository-base";
 
 export class RepositoryFactory {
+  private readonly cache: Record<string, RepositoryBase> = {};
+
   constructor(private database: KnexDatabase, private logger: Logger) {}
 
   public create(name: keyof typeof OidcTypes) {
+    if (this.cache[name]) return this.cache[name];
+
     const [Repository] = Object.entries(repositories)
       .filter(([className]) => className.startsWith(name))
       .map(([_, Ctor]) => Ctor);
 
-    return new Repository(this.database, this.logger);
+    const repository = new Repository(this.database, this.logger);
+
+    this.cache[name] = repository;
+
+    return repository;
   }
 }
